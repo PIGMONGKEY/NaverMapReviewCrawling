d21c7b04e5f654898c783b295a72295a3506d4e2
LeeWonHee <pigmong02020202@gmail.com>
2023-08-08 22:17:20 +0900

    크롤링 성공 - 한글이 깨짐 네이버 플레이스로 접속해야 함

---
+++ Crawling.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://pcmap.place.naver.com/restaurant/1231684190/review/visitor?entry=bmp&from=map&fromPanelNum=" \
+      "1&ts=1691500494146"
+# headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+#                          "/115.0.0.0 Safari/537.36"}
+response = requests.get(url)
+print(response.status_code)
+# print(response.text)
+soup = BeautifulSoup(response.text, "html.parser")
+
+value = soup.find("span", {"class": "zPfVt"})
+print(value)
