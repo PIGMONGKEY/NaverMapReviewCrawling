b1d875a0288f822591d98e62c6b73a55d9aea81d
LeeWonHee <pigmong02020202@gmail.com>
2023-08-09 11:33:18 +0900

    더보기 버튼을 눌러서 모든 리뷰를 가져오기 위해서는 selenium을 사용해야 한다

--- Crawling.py
+++ Crawling.py
@@ -5,10 +5,13 @@ url = "https://pcmap.place.naver.com/restaurant/1231684190/review/visitor?entry=
       "1&ts=1691500494146"
 # headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
 #                          "/115.0.0.0 Safari/537.36"}
+
 response = requests.get(url)
 print(response.status_code)
-# print(response.text)
 soup = BeautifulSoup(response.content.decode('utf-8', 'ignore'), features="html.parser")
 
 value = soup.find("span", {"class": "zPfVt"})
+print(value)
+
+value = soup.find_all("span", {"class": "zPfVt"})
 print(value)
