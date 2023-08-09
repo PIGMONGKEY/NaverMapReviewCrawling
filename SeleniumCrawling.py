b53c87a458f487096d77b88acaa2b170f3faaf06
LeeWonHee <pigmong02020202@gmail.com>
2023-08-10 01:43:20 +0900

    리뷰 저장 길이 제한 40으로 늘림

--- SeleniumCrawling.py
+++ SeleniumCrawling.py
@@ -33,7 +33,7 @@ while 1:
 
 for s in driver.find_elements(By.CSS_SELECTOR, "span.zPfVt"):
     print(s.text)
-    if s.text.__len__() > 20:
+    if s.text.__len__() > 40:
         file.write(s.text + "\n\n")
 
 file.close()
