96eb87602aea0121f11de4819bdec4c7267d4c7f
LeeWonHee <pigmong02020202@gmail.com>
2023-08-11 17:22:42 +0900

    상호명으로 검색하여 리뷰 스크래핑 성공

--- NaverMapSearch.py
+++ NaverMapSearch.py
@@ -1,15 +1,54 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-class NaverAPI:
-    def geocoding(self, addr):
-        client_id = "n4vlz6cuy2"
-        client_secret = "lJKtQFtBLyBOyyLLfQ2GXJ6VD6HJlS8VMkAyqrtW"
+search_keyword = "버거킹 의정부용현DT점"
 
-        url = f"https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?{addr}"
-        headers = {"X-NCP-APIGW-API-KEY-ID": client_id,
-                   "X-NCP-APIGW-API-KEY": client_secret}
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(f"https://map.naver.com/v5/search/{search_keyword}/place")
 
-        response = requests.get(url=url, headers=headers)
+time.sleep(2)
 
-        if response.status_code == 200:
-            
+driver.switch_to.frame("searchIframe")
+driver.execute_script('document.querySelector("#_pcmap_list_scroll_container > ul > li > div.CHC5F > a > div").click()')
+
+time.sleep(3)
+
+driver.switch_to.default_content()
+driver.switch_to.frame("entryIframe")
+driver.execute_script('document.querySelector("#app-root > div > div > div > div.place_fixed_maintab > div > div > div'
+                      ' > div > a:nth-child(3) > span").click()')
+
+place_code = driver.current_url.split("/")[-1].split("?")[0]
+place_url = f"https://pcmap.place.naver.com/restaurant/{place_code}/review/visitor"
+
+driver.get(place_url)
+
+time.sleep(3)
+
+driver.switch_to.default_content()
+
+try:
+    file = open("./CrawlingTest.txt", "w")
+except:
+    print("There is no such file...")
+    exit(-1)
+
+while 1:
+    try:
+        more_button = driver.find_element(By.CSS_SELECTOR, "a.fvwqf")
+        more_button.click()
+        time.sleep(1)
+    except:
+        break
+
+for s in driver.find_elements(By.CSS_SELECTOR, "span.zPfVt"):
+    print(s.text)
+    if s.text.__len__() > 0:
+        file.write(s.text + "\n\n")
+
+file.close()
+driver.close()
