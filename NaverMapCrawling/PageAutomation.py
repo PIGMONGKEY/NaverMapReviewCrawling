579b861c49c73d0324e7e3f96bfe801d9bff0be5
LeeWonHee <pigmong02020202@gmail.com>
2023-08-11 20:31:06 +0900

    PageAutomation.py Test Done

--- NaverMapCrawling/PageAutomation.py
+++ NaverMapCrawling/PageAutomation.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
@@ -15,7 +16,7 @@ def change_frame(driver, frame_id):
     driver.switch_to.frame(frame_id)
 
 
-def search_and_move_to_naver_place_page(driver, search_url):
+def get_place_code(driver, search_url):
     one_result_click_js = 'document.querySelector("#_pcmap_list_scroll_container > ul > li > div.CHC5F > a ' \
                           '> div").click()'
     many_result_click_js = 'document.querySelector("#_pcmap_list_scroll_container > ul > li:nth-child(1) > ' \
@@ -31,3 +32,28 @@ def search_and_move_to_naver_place_page(driver, search_url):
         driver.execute_script(many_result_click_js)
 
     time.sleep(3)
+
+    change_frame(driver, "entryIframe")
+    driver.execute_script('document.querySelector("#app-root > div > div > div > div.place_fixed_maintab > div > div'
+                          ' > div > div > a:nth-child(3) > span").click()')
+
+    return driver.current_url.split("/")[-1].split("?")[0]
+
+
+def move_to_review_page(driver, place_code):
+    place_url = f"https://pcmap.place.naver.com/restaurant/{place_code}/review/visitor"
+
+    driver.get(place_url)
+    time.sleep(3)
+
+
+def click_more_button(driver):
+    more_button_css = "a.fvwqf"
+
+    while 1:
+        try:
+            more_button = driver.find_element(By.CSS_SELECTOR, more_button_css)
+            more_button.click()
+            time.sleep(1)
+        except:
+            break
--- Test/NaverMapSearch.py
+++ Test/NaverMapSearch.py
@@ -9,44 +9,15 @@ from NaverMapCrawling.FileIO import *
 from NaverMapCrawling.PageAutomation import *
 
 search_keyword = "의정부 미술도서관"
+search_url = f"https://map.naver.com/v5/search/{search_keyword}/place"
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(f"https://map.naver.com/v5/search/{search_keyword}/place")
-
-time.sleep(2)
-
-change_frame(driver, "searchIframe")
-
-try:
-    driver.execute_script('document.querySelector("#_pcmap_list_scroll_container > ul > li > div.CHC5F > a '
-                          '> div").click()')
-except:
-    driver.execute_script('document.querySelector("#_pcmap_list_scroll_container > ul > li:nth-child(1) > '
-                          'div.qbGlu > div.ouxiq.icT4K > a:nth-child(1) > div").click()')
-
-time.sleep(3)
-
-change_frame(driver, "entryIframe")
-
-driver.execute_script('document.querySelector("#app-root > div > div > div > div.place_fixed_maintab > div > div > div'
-                      ' > div > a:nth-child(3) > span").click()')
-
-place_code = driver.current_url.split("/")[-1].split("?")[0]
-place_url = f"https://pcmap.place.naver.com/restaurant/{place_code}/review/visitor"
-
-driver.get(place_url)
-
-time.sleep(3)
+driver = driver_init()
+place_code = get_place_code(driver, search_url)
+move_to_review_page(driver, place_code)
 
 driver.switch_to.default_content()
 
-while 1:
-    try:
-        more_button = driver.find_element(By.CSS_SELECTOR, "a.fvwqf")
-        more_button.click()
-        time.sleep(1)
-    except:
-        break
+click_more_button(driver)
 
 review_write(search_keyword, driver)
 
