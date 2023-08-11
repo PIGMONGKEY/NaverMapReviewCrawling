8fac818f47f6ec47ed565572e1c200e1ab457217
LeeWonHee <pigmong02020202@gmail.com>
2023-08-11 19:46:10 +0900

    FileIO.review_write Created

--- NaverMapCrawling/FileIO.py
+++ NaverMapCrawling/FileIO.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
 def open_file(brand_name):
     return open(f"../Reviews/{brand_name}", "w")
 
@@ -8,3 +11,9 @@ def write_file(file, review):
 
 def close_file(file):
     file.close()
+
+
+def review_write(driver, file):
+    for s in driver.find_elements(By.CSS_SELECTOR, "span.zPfVt"):
+        if s.text >= 5:
+            write_file(file, s.text)
