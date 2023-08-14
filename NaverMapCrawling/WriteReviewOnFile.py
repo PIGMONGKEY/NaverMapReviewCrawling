8e9c7b3a28078bcaad4964860a8b1672bca61648
LeeWonHee <pigmong02020202@gmail.com>
2023-08-14 17:09:37 +0900

    WriteReviewOnFile.py 주석 추가

--- NaverMapCrawling/WriteReviewOnFile.py
+++ NaverMapCrawling/WriteReviewOnFile.py
@@ -1,18 +1,22 @@
 from selenium.webdriver.common.by import By
 
 
+# 검색어 명으로 파일 생성
 def open_file(brand_name):
     return open(f"../Reviews/{brand_name}.txt", "w")
 
 
+# 리뷰를 txt 파일에 작성
 def write_file(file, review):
     file.write(review + "\n\n")
 
 
+# 파일 닫기
 def close_file(file):
     file.close()
 
 
+# 모든 리뷰를 불러와서 파일에 작성
 def review_write(brand_name, driver):
     file = open_file(brand_name)
 
--- Test/NaverMapSearch.py
+++ Test/NaverMapSearch.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-from NaverMapCrawling.FileIO import *
+from NaverMapCrawling.WriteReviewOnFile import *
 from NaverMapCrawling.PageAutomation import *
 
 if __name__ == '__main__':
